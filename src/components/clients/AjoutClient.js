722bf3d2d9cb5c504476877f1faf3f8ea1055fab
System Administrator <root@MacMikhail.local>
2022-05-10 10:07:00 -0400

    ajout de la methode saveOrUpdateClient

--- src/components/clients/AjoutClient.js
+++ src/components/clients/AjoutClient.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link, useNavigate, useParams} from 'react-router-dom';
+import ClientService from '../services/ClientService'
 
 const AjoutClient = () => {
+
+    let navigation = createBrowserHistory();
+
+    const [nom, setNom] = useState('')
+    const [prenom, setPrenom] = useState('')
+    const [adresse, setAdresse] = useState('')
+
+    const {id} = useParams();
+
+    const saveOrUpdateClient = (e) => {
+        e.preventDefault();
+
+        const client = {nom, prenom, adresse}
+
+        if (id) {
+            ClientService.updateClient(id, client).then((response) => {
+            }).catch(error => {
+                console.log(error)
+            })
+        } else {
+            ClientService.createClient(client).then((response) => {
+                console.log(response.data)
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    }
 
     return (
         <div>
